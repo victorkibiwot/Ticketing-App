a0af4c2712ceed1819004b58aef15be0477b3f9e
Ian-Ag-96 <agumeian96@gmail.com>
2025-07-30 11:09:37 +0300

    Passive config changes

--- utils/axios.js
+++ utils/axios.js
@@ -2,13 +2,13 @@ const axios = require('axios');
 
 // Create an Axios instance to persist cookies
 const axiosInstance = axios.create({
-  baseURL: process.env.GCP_URL,
+  baseURL: process.env.AUTH_URL,
   withCredentials: true // Enable sending cookies with requests
 });
 
 // Create an Axios instance to persist cookies
 const axiosInstance2 = axios.create({
-  baseURL: process.env.GCP_URL2,
+  baseURL: process.env.TICKET_URL,
   withCredentials: true // Enable sending cookies with requests
 });
 
